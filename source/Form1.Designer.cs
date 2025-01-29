f18c86a4b1af42796dcace118937096790e833b1
MISTERPUG51 <100051642+MISTERPUG51@users.noreply.github.com>
2025-01-28 21:24:33 -0500

    upload source code

---
+++ source/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace CrASH_SDK_Helper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compileProgramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crASHDocumentationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cRASHTXTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cRASHPRGTXTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aSMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.importAincludeFileINCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.compileProgramToolStripMenuItem,
+            this.importAincludeFileINCToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // compileProgramToolStripMenuItem
+            // 
+            this.compileProgramToolStripMenuItem.Name = "compileProgramToolStripMenuItem";
+            this.compileProgramToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.compileProgramToolStripMenuItem.Text = "Compile Program";
+            this.compileProgramToolStripMenuItem.Click += new System.EventHandler(this.compileProgramToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.crASHDocumentationToolStripMenuItem,
+            this.aSMToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // crASHDocumentationToolStripMenuItem
+            // 
+            this.crASHDocumentationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cRASHTXTToolStripMenuItem,
+            this.cRASHPRGTXTToolStripMenuItem});
+            this.crASHDocumentationToolStripMenuItem.Name = "crASHDocumentationToolStripMenuItem";
+            this.crASHDocumentationToolStripMenuItem.Size = new System.Drawing.Size(323, 22);
+            this.crASHDocumentationToolStripMenuItem.Text = "CrASH Documentation";
+            // 
+            // cRASHTXTToolStripMenuItem
+            // 
+            this.cRASHTXTToolStripMenuItem.Name = "cRASHTXTToolStripMenuItem";
+            this.cRASHTXTToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.cRASHTXTToolStripMenuItem.Text = "CRASH.TXT";
+            this.cRASHTXTToolStripMenuItem.Click += new System.EventHandler(this.cRASHTXTToolStripMenuItem_Click);
+            // 
+            // cRASHPRGTXTToolStripMenuItem
+            // 
+            this.cRASHPRGTXTToolStripMenuItem.Name = "cRASHPRGTXTToolStripMenuItem";
+            this.cRASHPRGTXTToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.cRASHPRGTXTToolStripMenuItem.Text = "CRASHPRG.TXT";
+            this.cRASHPRGTXTToolStripMenuItem.Click += new System.EventHandler(this.cRASHPRGTXTToolStripMenuItem_Click);
+            // 
+            // aSMToolStripMenuItem
+            // 
+            this.aSMToolStripMenuItem.Name = "aSMToolStripMenuItem";
+            this.aSMToolStripMenuItem.Size = new System.Drawing.Size(323, 22);
+            this.aSMToolStripMenuItem.Text = "ASM Programming Guide (on karma.ticalc.org)";
+            this.aSMToolStripMenuItem.Click += new System.EventHandler(this.aSMToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(323, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 411);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Assembly Files|*.asm";
+            this.openFileDialog1.Title = "Select an assembly file to open.";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Assembly Files|*.asm";
+            this.saveFileDialog1.Title = "Chose or create a file to save your program to.";
+            // 
+            // importAincludeFileINCToolStripMenuItem
+            // 
+            this.importAincludeFileINCToolStripMenuItem.Name = "importAincludeFileINCToolStripMenuItem";
+            this.importAincludeFileINCToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.importAincludeFileINCToolStripMenuItem.Text = "Import a #include file (.INC)";
+            this.importAincludeFileINCToolStripMenuItem.Click += new System.EventHandler(this.importAincludeFileINCToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "CrASH SDK Helper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem crASHDocumentationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cRASHTXTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cRASHPRGTXTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem aSMToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compileProgramToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem importAincludeFileINCToolStripMenuItem;
+    }
+}
+
---
+++ source/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CrASH_SDK_Helper
+{
+    public partial class Form1 : Form
+    {
+        public string currentFile = "";
+        private void SaveAs()
+        {
+            saveFileDialog1.ShowDialog();
+            currentFile = saveFileDialog1.FileName;
+            if (System.IO.Path.GetFileNameWithoutExtension(currentFile).Length > 8)
+            {
+                MessageBox.Show("The file name (not including the extension) must be less than 8 characters. The CrASH SDK does not work properly with file names with more than 8 characters.", caption: "Error: Filename too long");
+                SaveAs();
+            } else
+            {
+                this.Text = "CrASH SDK Helper - " + currentFile;
+                System.IO.File.WriteAllText(currentFile, richTextBox1.Text);
+                saveToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private void Save()
+        {
+            this.Text = "CrASH SDK Helper - " + currentFile;
+            System.IO.File.WriteAllText(currentFile, richTextBox1.Text);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            saveToolStripMenuItem.Enabled = false;
+        }
+
+        private void cRASHTXTToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad.exe", "CrASHSDK/CRASH.TXT");
+        }
+
+        private void cRASHPRGTXTToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad.exe", "CrASHSDK/CRASHPRG.TXT");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("This will discard any unsaved changes in the currently open file. Do you still want to continue?", "Warning", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                richTextBox1.Text = "";
+                currentFile = "";
+                this.Text = "CrASH SDK Helper - Unsaved";
+                saveToolStripMenuItem.Enabled = false;
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Save();
+            
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveAs();
+        }
+
+        private void aSMToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://karma.ticalc.org/guide/contents.html");
+        }
+
+        private void compileProgramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (currentFile == "")
+            {
+                MessageBox.Show("You need to save this file before you can compile it.");
+            }
+            else
+            {
+                DialogResult dialogResult = MessageBox.Show("All changes must be saved in order to compile your program. Do you want to save and compile your program?", "Do you want to save?", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Save();
+                    saveFileDialog1.Title = "Chose or create an 82P file to save your compiled program to.";
+                    saveFileDialog1.Filter = "TI-82 Program File|*.82P";
+                    saveFileDialog1.ShowDialog();
+                    saveFileDialog1.Filter = "Assembly Files|*.asm";
+                    saveFileDialog1.Title = "Chose or create a file to save your program to.";
+                    string outputFile = saveFileDialog1.FileName;
+                    string currentdir = AppDomain.CurrentDomain.BaseDirectory;
+                    string asmFileInCrashFolder = currentdir + "/" + System.IO.Path.GetFileName(currentFile);
+                    if (System.IO.File.Exists(asmFileInCrashFolder))
+                    {
+                        System.IO.File.Delete(asmFileInCrashFolder);
+                    }
+                    System.IO.File.Copy(currentFile,asmFileInCrashFolder);
+                    System.Environment.CurrentDirectory = currentdir;
+
+                    if (System.IO.File.Exists(System.IO.Path.GetDirectoryName(asmFileInCrashFolder) + "/" + System.IO.Path.GetFileNameWithoutExtension(asmFileInCrashFolder) + ".82p"))
+                    {
+                        System.IO.File.Delete(System.IO.Path.GetDirectoryName(asmFileInCrashFolder) + "/" + System.IO.Path.GetFileNameWithoutExtension(asmFileInCrashFolder) + ".82p");
+                    }
+
+
+                    var process = Process.Start('"' + currentdir + "\\CRASM.BAT" + '"', System.IO.Path.GetFileNameWithoutExtension(asmFileInCrashFolder));
+                    process.WaitForExit();
+                    if (System.IO.File.Exists(System.IO.Path.GetDirectoryName(asmFileInCrashFolder) + "/" + System.IO.Path.GetFileNameWithoutExtension(asmFileInCrashFolder) + ".82p"))
+                    {
+                        if (System.IO.File.Exists(outputFile))
+                        {
+                            System.IO.File.Delete(outputFile);
+                        }
+                        System.IO.File.Copy(System.IO.Path.GetDirectoryName(asmFileInCrashFolder) + "/" + System.IO.Path.GetFileNameWithoutExtension(asmFileInCrashFolder) + ".82p", outputFile);
+                        MessageBox.Show("The program was successfully compiled.\n\nThe .82P file for your program is:\n" + outputFile);
+                    } else
+                    {
+                        MessageBox.Show("An error occured during compilation.\n\nThe command prompt window that just closed contains information about the error(s).\nTo see it again, compile the program again.", "Error");
+                    }
+                    
+
+
+                    //System.IO.Path.GetDirectoryName(currentFile) + System.IO.Path.GetFileNameWithoutExtension(currentFile)
+                }
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("CrASH SDK Helper 1.0\n\n©2025 MISTERPUG51\n\nFor more information, visit the Github repository:\nhttps://github.com/MISTERPUG51/CrASHSDKHelper", caption: "About");
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            currentFile = openFileDialog1.FileName;
+            if (System.IO.Path.GetFileNameWithoutExtension(currentFile).Length > 8)
+            {
+                MessageBox.Show("The file name (not including the extension) must be less than 8 characters. Rename the file and try opening it again. The CrASH SDK does not work properly with file names with more than 8 characters.", caption: "Error: Filename too long");
+            }
+            else
+            {
+                this.Text = "CrASH SDK Helper - " + currentFile;
+                richTextBox1.Text = "Loading " + currentFile;
+                richTextBox1.Text = System.IO.File.ReadAllText(currentFile);
+                saveToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private void importAincludeFileINCToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Select a .INC file to import.";
+            openFileDialog1.Filter = ".INC Files|*.inc";
+            openFileDialog1.ShowDialog();
+            openFileDialog1.Filter = "Assembly Files|*.asm";
+            openFileDialog1.Title = "Select an assembly file to open.";
+            System.IO.File.Copy(openFileDialog1.FileName, AppDomain.CurrentDomain.BaseDirectory + openFileDialog1.SafeFileName);
+            MessageBox.Show("File successfully imported. The imported file is located at:\n" + AppDomain.CurrentDomain.BaseDirectory + openFileDialog1.SafeFileName);
+        }
+
+        private void visitWebsiteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://misterpug51.github.io/lnk/crashsdkhelper/");
+        }
+
+        private void visitGithubRepositoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/MISTERPUG51/crashsdkhelper/");
+        }
+    }
+}
